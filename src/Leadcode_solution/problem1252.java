30f4cf53da7a5e8945c54919c53556f1cd9cc12e
chandru07072007 <chandru07072007@gmail.com>
2026-07-07 00:09:26 +0530

    add problem1252

---
+++ src/Leadcode_solution/problem1252.java
@@ -0,0 +1,54 @@
+package Leadcode_solution;
+
+import java.util.Arrays;
+
+public class problem1252 {
+
+    public static void main(String[] args) {
+
+        int m = 2;
+        int n = 3;
+
+        int[][] indices = {
+                {0, 1},
+                {1, 1}
+        };
+
+
+        int[][] matrix = new int[m][n];
+
+
+        for (int i = 0; i < indices.length; i++) {
+
+            int row = indices[i][0];
+            int col = indices[i][1];
+
+
+            for (int j = 0; j < n; j++) {
+                matrix[row][j]++;
+            }
+
+
+            for (int j = 0; j < m; j++) {
+                matrix[j][col]++;
+            }
+        }
+
+
+        System.out.println("Matrix:");
+        System.out.println(Arrays.deepToString(matrix));
+
+
+        int oddCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+
+        System.out.println("Number of odd cells: " + oddCount);
+    }
+}
